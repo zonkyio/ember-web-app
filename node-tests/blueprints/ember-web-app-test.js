ab225706b9863571d30db06526b8ad32bc2bf9f8
Jan Bobisud <me@bobisjan.com>
2019-06-27 11:07:37 +0200

    Tests cleanup

--- node-tests/blueprints/ember-web-app-test.js
+++ node-tests/blueprints/ember-web-app-test.js
@@ -1,43 +1,44 @@
 'use strict';
+const { expect } = require('ember-cli-blueprint-test-helpers/chai');
+const {
+  setupTestHooks,
+  emberNew,
+  emberGenerate,
+  emberGenerateDestroy,
+  emberDestroy,
+} = require('ember-cli-blueprint-test-helpers/helpers');
 
-const blueprintHelpers = require('ember-cli-blueprint-test-helpers/helpers');
-const setupTestHooks = blueprintHelpers.setupTestHooks;
-const emberNew = blueprintHelpers.emberNew;
-const emberGenerate = blueprintHelpers.emberGenerate;
-const emberGenerateDestroy = blueprintHelpers.emberGenerateDestroy;
-const emberDestroy = blueprintHelpers.emberDestroy;
-
-const expect = require('ember-cli-blueprint-test-helpers/chai').expect;
-
-describe('Blueprints: ember generate and destroy ember-web-app', function() {
+describe('Blueprints', function() {
   setupTestHooks(this);
 
-  it('generates config/manifest.js file', function() {
-    let args = ['ember-web-app', 'foo'];
+  describe('ember generate', function() {
+    it('generates config/manifest.js file', function() {
+      let args = ['ember-web-app', 'foo'];
 
-    return emberNew().then(() =>
-      emberGenerateDestroy(args, file => {
-        expect(file('config/manifest.js'))
-          .to.contain('name: "my-app"')
-          .to.contain('short_name: "my-app"')
-          .to.contain('display: "standalone"');
-      })
-    );
-  });
+      return emberNew().then(() =>
+        emberGenerateDestroy(args, file => {
+          expect(file('config/manifest.js'))
+            .to.contain('name: "my-app"')
+            .to.contain('short_name: "my-app"')
+            .to.contain('display: "standalone"');
+        })
+      );
+    });
 
-  it('adds manifest link & browserconfig meta into app/index.html file', function() {
-    let args = ['ember-web-app', 'foo'];
+    it('adds manifest link & browserconfig meta into app/index.html file', function() {
+      let args = ['ember-web-app', 'foo'];
 
-    return emberNew().then(() =>
-      emberGenerate(args, file => {
-        expect(file('app/index.html'))
-          .to.contain(
-            '<link rel="manifest" src="{{rootURL}}manifest.webmanifest">'
-          )
-          .to.contain(
-            '<meta name="msapplication-config" content="{{rootURL}}browserconfig.xml">'
-          );
-      }).then(() => emberDestroy(args))
-    );
+      return emberNew().then(() =>
+        emberGenerate(args, file => {
+          expect(file('app/index.html'))
+            .to.contain(
+              '<link rel="manifest" src="{{rootURL}}manifest.webmanifest">'
+            )
+            .to.contain(
+              '<meta name="msapplication-config" content="{{rootURL}}browserconfig.xml">'
+            );
+        }).then(() => emberDestroy(args))
+      );
+    });
   });
 });
--- node-tests/unit/index-test.js
+++ node-tests/unit/index-test.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const assert = require('assert');
 const pristineIndex = require('../../index');
 
@@ -14,56 +13,67 @@ function createIndex() {
   });
 }
 
-describe('Unit: index', function() {
-  describe('contentFor()', function() {
-    it('returns empty when section is other than "head"', function() {
-      let index = createIndex();
+describe('Unit', function() {
+  describe('index', function() {
+    describe('contentFor()', function() {
+      it('returns empty when section is other than "head"', function() {
+        let index = createIndex();
 
-      assert.strictEqual(
-        index.contentFor('head-footer', { rootURL: '/' }),
-        undefined
-      );
-    });
+        assert.strictEqual(index.contentFor('head-footer'), undefined);
+      });
 
-    it('returns apple meta tags', function() {
-      let expected = '<meta name="apple-mobile-web-app-capable" content="yes">';
-      let index = createIndex();
+      it('returns apple meta tags', function() {
+        let index = createIndex();
 
-      assert.ok(index.contentFor('head', { rootURL: '/' }).includes(expected));
-    });
+        assert.ok(
+          index
+            .contentFor('head')
+            .includes(
+              '<meta name="apple-mobile-web-app-capable" content="yes">'
+            )
+        );
+      });
 
-    it('returns apple link tags', function() {
-      let expected =
-        '<link rel="apple-touch-icon" href="/foo/bar.png" sizes="180x180">';
-      let index = createIndex();
+      it('returns apple link tags', function() {
+        let index = createIndex();
 
-      index.manifest.configuration = {
-        icons: [
-          {
-            src: '/foo/bar.png',
-            sizes: '180x180',
-          },
-        ],
-      };
+        index.manifest.configuration = {
+          icons: [
+            {
+              src: '/foo/bar.png',
+              sizes: '180x180',
+            },
+          ],
+        };
 
-      assert.ok(index.contentFor('head', { rootURL: '/' }).includes(expected));
-    });
+        assert.ok(
+          index
+            .contentFor('head')
+            .includes(
+              '<link rel="apple-touch-icon" href="/foo/bar.png" sizes="180x180">'
+            )
+        );
+      });
 
-    it('returns safari pinned tab link tags', function() {
-      let expected = '<link rel="mask-icon" href="/foo/bar.svg" color="red">';
-      let index = createIndex();
+      it('returns safari pinned tab link tags', function() {
+        let index = createIndex();
 
-      index.manifest.configuration = {
-        icons: [
-          {
-            src: '/foo/bar.svg',
-            safariPinnedTabColor: 'red',
-            targets: ['safari-pinned-tab'],
-          },
-        ],
-      };
+        index.manifest.configuration = {
+          icons: [
+            {
+              src: '/foo/bar.svg',
+              safariPinnedTabColor: 'red',
+              targets: ['safari-pinned-tab'],
+            },
+          ],
+        };
 
-      assert.ok(index.contentFor('head', { rootURL: '/' }).includes(expected));
+        assert.ok(
+          index
+            .contentFor('head')
+            .includes('<link rel="mask-icon" href="/foo/bar.svg" color="red">')
+        );
+      });
     });
   });
 });

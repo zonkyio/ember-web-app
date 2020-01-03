7cb4965dc15a86101905b3d7d0429553fd7eae7b
Jan Bobisud <me@bobisjan.com>
2020-01-03 15:59:14 +0100

    Upgrade to Ember Octane
    - bump supported Ember.js to v3.8 or above

--- config/ember-try.js
+++ config/ember-try.js
@@ -11,24 +11,18 @@ module.exports = function() {
     return {
       scenarios: [
         {
-          name: 'ember-lts-2.18',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              'jquery-integration': true,
-            }),
-          },
+          name: 'ember-lts-3.8',
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0',
+              'ember-source': '~3.8.0',
             },
           },
         },
         {
-          name: 'ember-lts-3.4',
+          name: 'ember-lts-3.12',
           npm: {
             devDependencies: {
-              'ember-source': '~3.4.0',
+              'ember-source': '~3.12.0',
             },
           },
         },
@@ -66,6 +60,21 @@ module.exports = function() {
           npm: {
             devDependencies: {
               '@ember/jquery': '^0.5.1',
+            },
+          },
+        },
+        {
+          name: 'ember-classic',
+          env: {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({
+              'application-template-wrapper': true,
+              'default-async-observers': false,
+              'template-only-glimmer-components': false,
+            }),
+          },
+          npm: {
+            ember: {
+              edition: 'classic',
             },
           },
         },

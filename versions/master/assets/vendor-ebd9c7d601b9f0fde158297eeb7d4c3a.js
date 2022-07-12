feba3c03406a329ae80f548d3517906e200b054d
Tomster <tomster@emberjs.com>
2022-07-12 06:27:08 +0000

    Deployed 45e859b

--- versions/master/assets/vendor-ebd9c7d601b9f0fde158297eeb7d4c3a.js
+++ versions/master/assets/vendor-ebd9c7d601b9f0fde158297eeb7d4c3a.js
@@ -1,5 +1,5 @@
 window.EmberENV=function(e,t){for(var r in t)e[r]=t[r]
-return e}(window.EmberENV||{},{FEATURES:{},EXTEND_PROTOTYPES:{Date:!1},_APPLICATION_TEMPLATE_WRAPPER:!1,_DEFAULT_ASYNC_OBSERVERS:!0,_JQUERY_INTEGRATION:!1,_TEMPLATE_ONLY_GLIMMER_COMPONENTS:!0})
+return e}(window.EmberENV||{},{FEATURES:{},_APPLICATION_TEMPLATE_WRAPPER:!1,_DEFAULT_ASYNC_OBSERVERS:!0,_JQUERY_INTEGRATION:!1,_TEMPLATE_ONLY_GLIMMER_COMPONENTS:!0})
 var loader,define,requireModule,require,requirejs,runningTests=!1
 if(function(e){"use strict"
 function t(){var e=Object.create(null)
